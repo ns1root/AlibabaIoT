b89ac3d5ad500f658175c92b481e3d968e3e52a5
Yrota Labor <ns1root@outlook.com>
2020-03-02 15:13:31 +0900

    Add files via upload

--- application.py
+++ application.py
@@ -58,13 +58,13 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
   en_text = base64.b64encode(event.message.text)
-  request = PubRequest()
-  request.set_accept_format('json')
-  request.set_TopicFullName(ALICLOUD_IOT_TOPIC)
-  request.set_MessageContent(en_text)
-  request.set_ProductKey(ALICLOUD_IOT_PRODUCTKEY)
-  request.set_Qos("0")
-  response = client.do_action_with_exception(request)
+  #request = PubRequest()
+  #request.set_accept_format('json')
+  #request.set_TopicFullName(ALICLOUD_IOT_TOPIC)
+  #request.set_MessageContent(en_text)
+  #request.set_ProductKey(ALICLOUD_IOT_PRODUCTKEY)
+  #request.set_Qos("0")
+  #client.do_action_with_exception(request)
   #result = str(response)
   
   line_bot_api.reply_message(
